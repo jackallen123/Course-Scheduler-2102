8a7a85130af824c15f0562d2cc872b5bc359b3f3
jackallen123 <128607553+jackallen123@users.noreply.github.com>
2023-09-14 09:58:34 -0400

    Setup Vue.js project structure using 'vue create frontend'

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Hello World</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld'
+}
+</script>
+
+<style scoped>
+/* Custom styles go here */
+</style>
